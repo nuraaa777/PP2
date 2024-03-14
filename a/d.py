b46a19c65378f90c97d431858b881c9be1b9fab2
nuraaa777 <163147038+nuraaa777@users.noreply.github.com>
2024-03-14 13:26:08 +0500

    Update d.py

--- a/d.py
+++ a/d.py
@@ -1,9 +1,13 @@
 import re
 
-txt = input()
-regex = re.search('^[A-Z]{1}([a-z]*)$',txt)
-if regex:
-    print("True")
-else:
-    print("False")
+def find_sequences(text):
+    pattern = r'[A-Z][a-z]+'
+    sequences = re.findall(pattern, text)
+    return sequences
+
+# Test the function
+text = "This is a Sample Text With Sequences Like This One"
+sequences = find_sequences(text)
+print("Sequences found:", sequences)
+
 
