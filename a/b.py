d8844c106e4f8c0d3e0565a1a585042753721db4
nuraaa777 <163147038+nuraaa777@users.noreply.github.com>
2024-03-14 13:13:57 +0500

    Update b.py

--- a/b.py
+++ a/b.py
@@ -1,9 +1,11 @@
 import re
-
-txt = input()
-regex = re.search('^a(b){2,3}&',txt)
-if regex:
-    print("True")
-else:
-    print("False")
+def text_match(text):
+        patterns = 'ab{2,3}'
+        if re.search(patterns,  text):
+                return 'Found a match!'
+        else:
+                return('Not matched!')
+print(text_match("abmdmmdsn,s,s,abbb"))
+print(text_match("aabbbbbc"))
+print(text_match("ab"))
 
