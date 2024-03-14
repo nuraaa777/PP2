098518b91238f5e13ea9eeb0a1949511f0e9993a
nuraaa777 <163147038+nuraaa777@users.noreply.github.com>
2024-03-14 11:12:39 +0500

    Update 2.m.py

--- 4/math/2.m.py
+++ 4/math/2.m.py
@@ -4,4 +4,4 @@ Height = float(input('Height: '))
 wegth = float(input('Base, first value: '))
 length = float(input('Base, second value: '))
 trape = (wegth+length)*Height/2
-print(trape)
+print(trape)#trapecia
