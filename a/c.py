e2d7bd5bbce855da068d055c34ed77c5eccb81cd
nuraaa777 <163147038+nuraaa777@users.noreply.github.com>
2024-03-14 13:22:22 +0500

    Update c.py

--- a/c.py
+++ a/c.py
@@ -1,10 +1,14 @@
 import re
 
-txt = input()
-regex = re.search('[a-z]{1}(_){1}', txt)
-if regex:
-    print("True")
-else:
-    print("False")
+def find_sequences(text):
+    pattern = r'\b[a-z]+_[a-z]+\b'
+    sequences = re.findall(pattern, text)
+    return sequences
+
+# Test the function
+text = "hello_world is_a_sequence of_lowercase_letters"
+sequences = find_sequences(text)
+print("Sequences found:", sequences)
+
 
 
